de60d053789a189ec57e3d28377189f2a9695f97
siloudetanke <sil@sot.design>
2024-02-13 18:18:52 +0100

    Share modal fix

--- theme/template-parts/custom/custom-share-button.php
+++ theme/template-parts/custom/custom-share-button.php
@@ -9,13 +9,13 @@ $post_thumbnail_id = get_post_thumbnail_id(get_the_ID()); // Get the post thumbn
 
 if ($post_thumbnail_id) {
     $thumbnail = wp_get_attachment_image_src($post_thumbnail_id, 'medium');
-    $thumbnail_full = wp_get_attachment_image_src($post_thumbnail_id, 'large');
+    $thumbnail_full = wp_get_attachment_image_src($post_thumbnail_id, 'full');
     $thumbnail_url = $thumbnail[0];
     $thumbnail_url_full = $thumbnail_full[0];
 }
 ?>
 
-<div x-data @click="$dispatch('openmodal', { title: '<?php echo esc_html(get_the_title()) ?>', url: '<?php echo esc_url(get_permalink()) ?>', img: '<?php echo $thumbnail_url ?>', imgfull: '<?php echo $thumbnail_url_full ?>'})" class="border p-2 cursor-pointer">
+<div x-data @click="$dispatch('openmodal', { title: '<?php echo esc_html(get_the_title()) ?>', url: '<?php echo esc_url(get_permalink()) ?>', img: '<?php echo esc_url($thumbnail_url) ?>', imgfull: '<?php echo esc_url($thumbnail_url_full) ?>'})" class="border p-2 cursor-pointer">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
         <path d="m22 2-7 20-4-9-9-4Z" />
         <path d="M22 2 11 13" />
--- theme/template-parts/custom/custom-share-modal.php
+++ theme/template-parts/custom/custom-share-modal.php
@@ -12,7 +12,7 @@
     }
 </style>
 <!-- Add this code in your WordPress theme template -->
-<div x-data="{ isOpen: false, title: '', url: '', img: '' }" x-on:openmodal.window="isOpen = true, title = $event.detail['title'], url = $event.detail['url'], img = $event.detail['img'], imgfull = $event.detail['imgfull']">
+<div x-data="{ isOpen: false, title: '', url: '', img: '', imgfull: ''}" x-on:openmodal.window="isOpen = true, title = $event.detail['title'], url = $event.detail['url'], img = $event.detail['img'], imgfull = $event.detail['imgfull']">
     <!-- Modal -->
     <div x-cloak x-transition x-show="isOpen" transition class="fixed inset-0 overflow-y-auto z-50 ">
         <div class="flex items-center justify-center min-h-screen p-6">
@@ -37,14 +37,14 @@
                 <h2 class="uppercase text-2xl max-w-xs text-center font-semibold" x-text="title"></h2>
                 <ul class="text-sm">
                     <li class="mb-2">
-                        <button @click="shareTo('facebook', url, title, img)" class="button-outline w-full flex gap-2 items-center justify-center">
+                        <button @click="shareTo('facebook', url, title, img, imgfull)" class="button-outline w-full flex gap-2 items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                             </svg>
                             Facebook</button>
                     </li>
                     <li class="mb-2">
-                        <button @click="shareTo('whatsapp', url, title, img)" class="button-outline w-full flex gap-2 items-center justify-center">
+                        <button @click="shareTo('whatsapp', url, title, img, imgfull)" class="button-outline w-full flex gap-2 items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
                             </svg>
@@ -52,7 +52,7 @@
                         </button>
                     </li>
                     <li class="mb-2">
-                        <button @click="shareTo('pinterest', url, title, img)" class="button-outline w-full flex gap-2 items-center justify-center">
+                        <button @click="shareTo('pinterest', url, title, img, imgfull)" class="button-outline w-full flex gap-2 items-center justify-center">
                             <svg xmlns=" http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                 <line x1="12" x2="12" y1="17" y2="22" />
                                 <path d="M5 17h14v-1.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V6h1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4h1v4.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24Z" />
@@ -61,7 +61,7 @@
                         </button>
                     </li>
                     <li class="mb-2">
-                        <button @click="shareTo('twitter', url, title, img)" class="button-outline w-full flex gap-2 items-center justify-center">
+                        <button @click="shareTo('twitter', url, title, img, imgfull)" class="button-outline w-full flex gap-2 items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
                             </svg>
